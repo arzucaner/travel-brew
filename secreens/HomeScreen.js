7368631ea6030ce810f5f646bedd4aaf99a1cff3
Arzu Caner <arzuguneycaner@gmail.com>
2023-11-19 13:59:39 +0000

    Initial commit
    Added homescreen.js and loginscreen.js file.

---
+++ secreens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
---
+++ secreens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Login Screen</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
